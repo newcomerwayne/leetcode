b0c7c86497bf7e80f9de278f34f5128f97e9be74
newcomerwayne <87293210+newcomerwayne@users.noreply.github.com>
2022-01-18 15:19:28 +0800

    Add files via upload

---
+++ string/344. Reverse String.cpp
@@ -0,0 +1,30 @@
+Pointer:
+void reverseString(char* s, int sSize)
+{
+    int i;
+    char tmp;
+     for( i = 0; i < sSize; i++, sSize-- )
+     {
+        tmp = *(s + i);
+        *(s + i) = *(s + sSize - 1);
+        *(s + sSize - 1) = tmp;    
+     }
+    return s;
+}
+
+Array:
+void reverseString(char* s, int sSize)
+{
+    
+    int i = 0,j = sSize-1;
+    char temp;
+    
+    while(i < j){
+        
+        temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        
+        i++;j--;
+    }  
+}
